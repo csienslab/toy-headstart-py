b29ae48593577036f8fac2399c52d4dcdbe6cbcb
maple3142 <kirby741852963@gmail.com>
2023-10-24 23:36:02 +0800

    preliminary server implementation

---
+++ server.py
@@ -0,0 +1,158 @@
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import HTTPException
+from flask.json.provider import JSONProvider
+from apscheduler.schedulers.background import BackgroundScheduler
+import atexit, logging, base64, json
+from stage import Stage, Phase
+
+
+class RandomnessBeacon:
+    def __init__(self, logger: logging.Logger):
+        self.logger = logger
+        self.stages: list[Stage] = [Stage()]
+        self.interval_seconds = 10
+
+    @property
+    def current_stage(self):
+        return self.stages[-1]
+
+    @property
+    def current_stage_index(self):
+        return len(self.stages) - 1
+
+    def get_stage(self, stage_idx: int):
+        if not (0 <= stage_idx <= self.current_stage_index):
+            raise ValueError("invalid stage")
+        return self.stages[stage_idx]
+
+    def get_stage_after_phase(self, stage_idx: int, phase: Phase):
+        stage = self.get_stage(stage_idx)
+        if stage.phase < phase:
+            raise ValueError("not in correct phase")
+        return stage
+
+    def contribute(self, x: bytes):
+        self.logger.debug(
+            f"Contribution received",
+            extra={"x": x.hex(), "stage": self.current_stage_index},
+        )
+        data_idx = self.current_stage.contribute(x)
+        stage_idx = self.current_stage_index
+        return stage_idx, data_idx
+
+    def next_stage(self):
+        self.logger.info(
+            f"Starting next stage", extra={"stage": self.current_stage_index + 1}
+        )
+        self.current_stage.stop_contribution()
+        self.stages.append(Stage())
+
+    def register_scheduler(self):
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(
+            func=self.next_stage, trigger="interval", seconds=self.interval_seconds
+        )
+        scheduler.start()
+        atexit.register(lambda: scheduler.shutdown())
+        self.scheduler = scheduler
+
+
+class Base64Encoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, bytes):
+            return base64.b64encode(o).decode()
+        return json.JSONEncoder.default(self, o)
+
+
+class CustomJSONProvider(JSONProvider):
+    def dumps(self, obj, **kwargs):
+        return json.dumps(obj, cls=Base64Encoder, **kwargs)
+
+    def loads(self, s, **kwargs):
+        return json.loads(s, **kwargs)
+
+
+app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
+app.json = CustomJSONProvider(app)
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps(
+        {
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        }
+    )
+    response.content_type = "application/json"
+    return response
+
+
+beacon = RandomnessBeacon(app.logger)
+beacon.register_scheduler()
+
+
+@app.get("/api/info")
+def info():
+    return jsonify(
+        {
+            "stage": beacon.current_stage_index,
+            "contributions": len(beacon.current_stage.data),
+        }
+    )
+
+
+@app.post("/api/contribute")
+def contribute():
+    try:
+        x = base64.b64decode(request.json["randomness"])
+    except:
+        return (
+            jsonify(
+                {"error": "no randomness provided or randomness isn't base64 encoded"}
+            ),
+            400,
+        )
+    stage_idx, data_idx = beacon.contribute(x)
+    return jsonify({"stage": stage_idx, "data_index": data_idx})
+
+
+@app.get("/api/stage/<int:stage_idx>")
+def stage(stage_idx):
+    stage = beacon.get_stage(stage_idx)
+    return jsonify(
+        {
+            "phase": stage.phase.name,
+            "contributions": len(stage.data),
+        }
+    )
+
+
+@app.get("/api/stage/<int:stage_idx>/accval")
+def accval(stage_idx):
+    stage = beacon.get_stage_after_phase(stage_idx, Phase.EVALUATION)
+    return jsonify({"value": stage.get_acc_val()})
+
+
+@app.get("/api/stage/<int:stage_idx>/accproof/<int:data_idx>")
+def accproof(stage_idx, data_idx):
+    stage = beacon.get_stage_after_phase(stage_idx, Phase.EVALUATION)
+    return jsonify({"value": stage.get_acc_proof(data_idx)})
+
+
+@app.get("/api/stage/<int:stage_idx>/vdfproof")
+def vdfproof(stage_idx):
+    stage = beacon.get_stage_after_phase(stage_idx, Phase.DONE)
+    return jsonify({"value": stage.get_vdf_proof()})
+
+
+@app.get("/api/stage/<int:stage_idx>/randomness")
+def randomness(stage_idx):
+    stage = beacon.get_stage_after_phase(stage_idx, Phase.DONE)
+    return jsonify({"value": stage.get_final_randomness()})
---
+++ stage.py
@@ -0,0 +1,98 @@
+from merkle_tree import MerkleHash, MerkleTreeAccumulator, SortedMerkleTreeAccumulator
+from rsa_accumulator import RSAAccumulator, RSAPrimeAccumulator
+from bqf_accumulator import BQFAccumulator, ChiaBQFAccumulator
+from abstract import AbstractVDF
+from toy_vdf import ToyVDF
+from chia_vdf import ChiaVDF
+from hashlib import sha256
+from enum import Enum
+from threading import Thread, Lock
+import sys, os, random
+
+# This implements https://www.ndss-symposium.org/wp-content/uploads/2022-234-paper.pdf special case L=1
+
+
+class Phase(Enum):
+    CONTRIBUTION = 1
+    EVALUATION = 2
+    DONE = 3
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+
+class Parameters:
+    # accumulator = RSAAccumulator.generate(1024)
+    # accumulator = RSAPrimeAccumulator.generate(1024)
+    # accumulator = BQFAccumulator.generate(1024)
+    # accumulator = ChiaBQFAccumulator.generate(1024)
+    accumulator = MerkleTreeAccumulator(MerkleHash(sha256))
+    # accumulator = SortedMerkleTreeAccumulator(MerkleHash(sha256))
+    T = 2**16
+    bits = 256
+    # vdf = ToyVDF(bits, T)
+    vdf = ChiaVDF(bits, T)
+
+
+class VDFComputation:
+    def __init__(self, vdf: AbstractVDF, challenge: bytes):
+        self.vdf = vdf
+        self.challenge = challenge
+        self.done = False
+        self.thread = Thread(target=self.run)
+        self.thread.start()
+
+    def run(self, callback=None):
+        self.proof = self.vdf.prove(self.challenge)
+        self.done = True
+        if callback:
+            callback()
+
+    def get(self):
+        if not self.done:
+            raise ValueError("not done yet")
+        return self.proof
+
+
+class Stage:
+    def __init__(self):
+        self.data: list[bytes] = [b"DUMMY VALUE"]  # to prevent some errors
+        self.phase = Phase.CONTRIBUTION
+
+    def contribute(self, x: bytes):
+        if self.phase != Phase.CONTRIBUTION:
+            raise ValueError("not in contribution phase")
+        self.data.append(x)
+        return len(self.data) - 1  # index of x in the data
+
+    def stop_contribution(self):
+        if self.phase != Phase.CONTRIBUTION:
+            raise ValueError("not in contribution phase")
+        self.phase = Phase.EVALUATION
+        self.acc = Parameters.accumulator.accumulate(self.data)
+        self.vdf = VDFComputation(
+            Parameters.vdf, Parameters.accumulator.get_bytes(self.get_acc_val())
+        )
+        self.vdf.run(callback=self.vdf_callback)
+
+    def vdf_callback(self):
+        self.phase = Phase.DONE
+
+    def get_acc_val(self):
+        if self.phase != Phase.EVALUATION:
+            raise ValueError("not in evaluation phase")
+        return Parameters.accumulator.get_accval(self.acc)
+
+    def get_acc_proof(self, data_index: int):
+        if self.phase != Phase.EVALUATION:
+            raise ValueError("not in evaluation phase")
+        return Parameters.accumulator.witgen(self.acc, self.data, data_index)
+
+    def get_vdf_proof(self):
+        if self.phase != Phase.DONE:
+            raise ValueError("not in done phase")
+        return self.vdf.get()
+
+    def get_final_randomness(self):
+        proof = self.get_vdf_proof()
+        return sha256(Parameters.vdf.extract_y(proof)).digest()

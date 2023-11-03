ac24495577e8714bc2bf4c0a4b175dee1d3810c5
maple3142 <kirby741852963@gmail.com>
2023-11-03 17:39:48 +0800

    add vdf perf test

---
+++ vdf_perf_test.py
@@ -0,0 +1,28 @@
+from chia_vdf import ChiaVDF
+import timeit, os
+
+
+def vdf_test(vdf: ChiaVDF):
+    challenge = os.urandom(8)
+    proof = vdf.prove(challenge)
+    assert vdf.verify(challenge, proof)
+    y = vdf.extract_y(proof)
+
+
+K = 3
+for bits in [1024]:
+    for T in range(16, 24):
+        vdf = ChiaVDF(bits, 1 << T)
+        t = timeit.timeit(lambda: vdf_test(vdf), number=K) / K
+        print(f"bits={bits}, T={T}, time={t}")
+
+"""
+bits=1024, T=16, time=0.5948997130035423
+bits=1024, T=17, time=1.1527132293170628
+bits=1024, T=18, time=2.248782481998205
+bits=1024, T=19, time=4.43606647234022
+bits=1024, T=20, time=8.68817146832589
+bits=1024, T=21, time=17.617124020995107
+bits=1024, T=22, time=34.3421061343397
+bits=1024, T=23, time=67.44681108933098
+"""
